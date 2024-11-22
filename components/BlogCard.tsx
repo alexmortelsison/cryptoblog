fabddc1573597525aa2a78800b1edd4e1e510299
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-11-22 20:58:41 +0900

    updated

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -8,7 +8,7 @@ export type BlogTypeCard = Blog;
 const BlogCard = ({ post }: { post: BlogTypeCard }) => {
   const { image, title, excerpt, _id } = post;
   return (
-    <li className="border border-muted-foreground dark:border-white rounded-lg hover:border-primary hover:text-primary hover:scale-105 hover:duration-300 duration-300 shadow-md dark:shadow-lg shadow-slate-500 dark:shadow-slate-400 mb-8">
+    <li className="border border-muted-foreground dark:border-white rounded-lg hover:border-primary hover:text-primary hover:scale-105 hover:duration-300 duration-300 shadow-md dark:shadow-lg shadow-slate-500 dark:shadow-slate-400 mb-7">
       <div className="rounded-lg h-[100px]">
         <img src={image} alt="" className="rounded-t-lg h-[200px] w-full" />
       </div>
